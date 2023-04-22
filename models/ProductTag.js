5e6139462dcd38843bddd7762fd2d5f0ba1d8369
Jimmys Mac <eadesj25@gmail.com>
2023-04-22 16:25:47 -0400

    models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,21 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "product",
+        key: "id",
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      }
+    }  
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,11 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [Product]
+    });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
+  
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
