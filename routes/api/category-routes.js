b5f3e56d2a275ce54a5efb290450fe792760f2c3
Jimmys Mac <eadesj25@gmail.com>
2023-05-01 22:17:56 -0400

    routes by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,9 +14,23 @@ router.get('/', async (req, res) => {
   }
 });
   
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [Product]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "You won't find a category with that id here"})
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
